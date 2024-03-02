051446aa48de494480d0bc52682fd1354c7f480e
Sharuk Abdul <80454595+sharukabdul@users.noreply.github.com>
2024-03-02 18:38:05 +0530

    portfolio-website

---
+++ script.js
@@ -0,0 +1,45 @@
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tabLink of tabLinks) {
+        tabLink.classList.remove("active-link");
+    }
+    for(tabContent of tabContents) {
+        tabContent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenu.style.left = "0";
+}
+
+function closemenu() {
+    sidemenu.style.left = "100%";
+}
+
+
+// Scroll to Top------------------
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
